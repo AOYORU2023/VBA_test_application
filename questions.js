134b15015cce2e6a98687f7a2a0b52e9a8e0c7f6
AOYORU2023 <152047943+AOYORU2023@users.noreply.github.com>
2024-07-21 04:22:51 +0900

    Update questions.js

--- questions.js
+++ questions.js
@@ -3,7 +3,7 @@ const quizData = [
     // 1-1 用語と概念
     {
         question: "VBAとは何の略称ですか？",
-        choices: ["Visual Basic Algorithm", "Visual Basic for Applications", "Visual Basic Analysis"],
+        choices: ["Visual Basic Algorithm", "Visual Basic for Applications", "Visual Basic nalysis"],
         correct: 1,
         explanation: "VBAは「Visual Basic for Applications」の略称です。これはMicrosoft Office製品に組み込まれたプログラミング言語です。"
     },
